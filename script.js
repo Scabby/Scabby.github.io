841360ef3b0a5560fcb9a8ad6fd2f2ecd2d4163a
Scabby <61923737+Scabby@users.noreply.github.com>
2021-08-20 10:38:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -28,11 +28,11 @@ function gen() {
 
     for(let h = 0; h < height; h++) {
         let row = make("div")
-        row.className = "row " + h
+        row.id  = "row " + h
 
         for(let w = 0; w < width; w++) {
-            let block       = make("span")
-            block.className = "block " + w
+            let block   = make("span")
+            block.id    = "block " + w
             row.appendChild(block)
 
             if(Math.pow(w - x, 2) + Math.pow(h - y, 2) <= radius * radius) {
