784c798d810f50c0f4fefcf88101cf1a18c94999
Scabby <61923737+Scabby@users.noreply.github.com>
2021-08-22 19:25:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
 function get(id)    { return document.getElementById(id) }
 function make(type) { return document.createElement(type) }
 
+function getBoardHeight() {
+    return Math.floor(
+        board.offsetHeight /
+        helperBlock.offsetHeight
+    )
+}
+
+function getBoardWidth() {
+    return Math.floor(
+        board.offsetWidth /
+        helperBlock.offsetWidth
+    )
+}
+
+function throttle(f, wait, args) {
+    let timeout
+
+    if(timeout) { return }
+
+    timeout = true
+    f.apply(null, args)
+    setTimeout(() => timeout = false , wait)
+}
+
+
+
 function getBlock(y, x) {
     let blocks = get(rowPre + y).children
-    
+
     for(let i = 0; i < blocks.length; i++) {
-        if(blocks[i].id == blockPre + x) {
-            return blocks[i]
+        if(blocks[i].children[0].id == blockPre + x) {
+            return blocks[i].children[0]
         }
     }
 }
 
-function update(element) {
-    let display             = element.style.display
-    element.style.display   = "none"
-    element.style.display   = display
-}
-
 function calcBlock(block, value) {
     let text, color
-    
+
     if(value < 70)      { text = ""; color = "" }
     else if(value < 75) { text = "."; color = "dark_grey" }
     else if(value < 85) { text = ","; color = "grey" }
     else if(value < 95) { text = ":"; color = "light_grey" }
     else if(value < 97) { text = ";"; color = "white" }
     else                { text = "!"; color = "red" }
-    
+
     block.textContent   = text
     block.className     = color
 }
@@ -42,35 +62,23 @@ function makePlayer(block) {
     block.className     = "blue"
 }
 
-function getBoardHeight() {
-    return Math.floor(
-        board.offsetHeight /
-        helperBlock.offsetHeight
-    )
-}
 
-function getBoardWidth() {
-    return Math.floor(
-        board.offsetWidth /
-        helperBlock.offsetWidth
-    )
-}
-
-function calcInMargin(size, margin) {
-    if(size <= margin * 2) {
-        return Math.floor(size / 2)
-    } else {
-        return Math.round(
-            Math.random() *
-            (size - margin * 2) +
-            margin
-        )
-    }
-}
 
 function gen() {
-    playerY = calcInMargin(boardHeight, margin)
-    playerX = calcInMargin(boardWidth, margin)
+    let calc = (size, margin) => {
+        if(size <= margin * 2) {
+            return Math.floor(size / 2)
+        } else {
+            return Math.round(
+                Math.random() *
+                (size - margin * 2) +
+                margin
+            )
+        }
+    }
+
+    playerY = calc(boardHeight, margin)
+    playerX = calc(boardWidth, margin)
 
     for(let h = 0; h < boardHeight; h++) {
         let row         = make("div")
@@ -78,10 +86,14 @@ function gen() {
         row.className   = rowPre
 
         for(let w = 0; w < boardWidth; w++) {
-            let block   = make("span")
-            block.id    = blockPre + w
+            let block       = make("div")
+            block.className = "block"
+
             row.appendChild(block)
-            
+            block.appendChild(make("p"))
+            let text    = block.children[0]
+            text.id     = blockPre + w
+
             let isWithinRadius = (
                 Math.pow(h - playerY, 2) +
                 Math.pow(w - playerX, 2) <=
@@ -90,19 +102,19 @@ function gen() {
 
             if(isWithinRadius) {
                 if(h == playerY && w == playerX) {
-                    makePlayer(block)
+                    makePlayer(text)
                 }
-            } else { genBlock(block) }
+            } else { genBlock(text) }
         }
-        
+
         board.appendChild(row)
     }
 }
 
 function regen() {
     board.replaceChildren()
+    board.style     = null
     board.className = "generating"
-    update(board)
 
     window.requestAnimationFrame(() => {
         boardHeight         = getBoardHeight()
@@ -111,90 +123,96 @@ function regen() {
         board.style.width   = boardWidth * helperBlock.offsetWidth
 
         gen()
-        
+
         board.className = "generated"
     })
 }
 
+
+
 function move(y, x) {
+    if(playerIsMoving) { return }
+
+    playerIsMoving = true
+
     let nextY = playerY + y
     let nextX = playerX + x
 
-    if(nextY >= 0 && nextY < boardHeight &&
-       nextX >= 0 && nextX < boardWidth)
-    {
-        calcBlock(playerBlock, 0)
-        
-        playerY = nextY
-        playerX = nextX
+    let withinY = nextY >= 0 && nextY < boardHeight
+    let withinX = nextX >= 0 && nextX < boardWidth
 
-        makePlayer(getBlock(playerY, playerX))
-    }
+    if(withinY && withinX) {
+        playerBlock.animate([
+            {
+                transform: "translate(" +
+                    x * helperBlock.offsetWidth + "px," +
+                    y * helperBlock.offsetHeight + "px)"
+            }
+        ], { duration: animationTime })
+
+        setTimeout(
+            () => {
+                calcBlock(playerBlock, 0)
+                playerY = nextY
+                playerX = nextX
+                makePlayer(getBlock(playerY, playerX))
+                playerIsMoving = false
+            },
+            animationTime
+        )
+    } else { playerIsMoving = false }
 }
 
-function moveUp()       { move(-1, 0);  console.log("up") }
-function moveDown()     { move(1, 0);   console.log("down") }
-function moveLeft()     { move(0, -1);  console.log("left") }
-function moveRight()    { move(0, 1);   console.log("right") }
+function moveUp()       { move(-1, 0) }
+function moveDown()     { move(1, 0) }
+function moveLeft()     { move(0, -1) }
+function moveRight()    { move(0, 1) }
 
-function throttle(func, wait = 500) {
-    let shouldWait
-    
-    return (... args) => {
-        if(!shouldWait) {
-            func.apply(this, args)
-            shouldWait = true
 
-            setTimeout(() => { shouldWait = false }, wait)
-        }
-    }
-}
-
-onclick = throttle(regen)
-
-function stopTouch(e) {
-    e.preventDefault()
-    e.stopImmediatePropagation()
-}
 
 window.addEventListener("touchstart", (e) => {
-    stopTouch(e)
-
-    lastY = e.touches[0].screenY
-    lastX = e.touches[0].screenX
-}, { passive: false })
+    lastSwipeY = e.touches[0].pageY
+    lastSwipeX = e.touches[0].pageX
+})
 
 window.addEventListener("touchmove", (e) => {
-    stopTouch(e)
-    
-    if(!lastY || !lastX) { return }
-    
-    parseSwipe = (e) => {
-        let currentY    = e.touches[0].screenY
-        let currentX    = e.touches[0].screenX
-        let deltaY      = currentY - lastY
-        let deltaX      = currentX - lastX
-        let isYSwipe    = Math.abs(deltaY) > Math.abs(deltaX)
+    let currentY    = e.touches[0].pageY
+    let currentX    = e.touches[0].pageX
+    let deltaY      = currentY - lastSwipeY
+    let deltaX      = currentX - lastSwipeX
 
-        if(isYSwipe) {
-            let isUp = currentY < lastY
+    let isYSwipe    = Math.abs(deltaY) > Math.abs(deltaX)
+    let yTooSmall   = Math.abs(deltaY) < swipeThreshold
+    let xTooSmall   = Math.abs(deltaX) < swipeThreshold
 
-            if(isUp)    { moveUp() }
-            else        { moveDown() }
-        } else {
-            let isLeft = currentX < lastX
+    if(yTooSmall || xTooSmall) { return }
 
-            if(isLeft)  { moveLeft() }
-            else        { moveRight() }
-        }
+    if(isYSwipe) {
+        let isUp = currentY < lastSwipeY
+
+        if(isUp)    { moveUp() }
+        else        { moveDown() }
+    } else {
+        let isLeft = currentX < lastSwipeX
+
+        if(isLeft)  { moveLeft() }
+        else        { moveRight() }
     }
 
-    throttle(parseSwipe, 100, e)
+    lastSwipeY = currentY
+    lastSwipeX = currentX
 }, { passive: false })
 
-window.addEventListener("touchend", (e) => {
-    stopTouch(e)
-}, { passive: false })
+window.addEventListener("touchend", () => {
+    lastSwipeY = null
+    lastSwipeX = null
+})
+
+
+
+onclick = () => {}
+
+
 
 onkeydown = (e) => {
     let key = e.key
@@ -207,19 +225,30 @@ onkeydown = (e) => {
     }
 }
 
+
+
 onload = () => {
     board          = get("board")
     helperBlock    = get("helper_block")
     regen()
 }
 
-radius      = 7.5
-margin      = 15
-rowPre      = "row"
-blockPre    = "block"
 
-let boardWidth,
+
+playerIsMoving  = false
+animationTime   = 60
+swipeThreshold  = 3
+radius          = 7.5
+margin          = 15
+rowPre          = "row"
+blockPre        = "block"
+
+let board,
+    boardWidth,
     boardHeight,
+    lastSwipeY,
+    lastSwipeX,
+    helperBlock,
+    playerBlock,
     playerY,
-    playerX,
-    playerBlock
+    playerX
