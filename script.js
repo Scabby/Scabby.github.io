8e802da5a87c8059cac5e67369abe387e24015b2
Scabby <61923737+Scabby@users.noreply.github.com>
2021-09-28 11:39:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -291,8 +291,8 @@ move_right  = false
 move_speed      = 1
 follow_distance = 150
 follow_ease     = 50
-leave_distance  = 10
-leave_ease      = 3
+leave_distance  = 5
+leave_ease      = 5
 
 touch_sensitivity   = 0.3
 touch_threshold     = 5
